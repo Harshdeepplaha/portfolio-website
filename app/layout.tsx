f0f6c262f0d0d2db4fb99cac169338d2bfa99dd6
harshdeep <harshdeepplaha05@gmail.com>
2024-09-04 14:55:11 -0500

    changes in the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ricardo | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  title: "Harshdeep Plaha | Personal Portfolio",
+  description: "Harshdeep is a Full stack developer and a Data Scientist.",
 };
 
 export default function RootLayout({
